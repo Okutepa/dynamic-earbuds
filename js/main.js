f0d67d0d2c892556ebd66fd5618f207e7f1b0f78
Okutepa <okutepa15@gmail.com>
2024-11-08 16:10:57 -0500

    Updated the Earbuds model on site
    In this commit, i updated the earbuds model on the website.

---
+++ js/main.js
@@ -0,0 +1,61 @@
+(() => {
+  console.log("IIFE Fired");
+
+  const infoHotspot = [
+    {
+      slot: "hotspot-1",
+      title: "Sweat &amp; Water Resistant",
+      description:
+        "The earbuds offer sweat and water resistance, ensuring durability for workouts and outdoor use.",
+      imgSrc: "images/water-resistant.svg",
+    },
+    {
+      slot: "hotspot-2",
+      title: "Ergonomic Design",
+      description:
+        "Designed for comfort, these earbuds provide a secure and ergonomic fit for extended listening sessions.",
+      imgSrc: "images/Ergonomic-design.svg",
+    },
+    {
+      slot: "hotspot-3",
+      title: "Noise Cancellation Technology",
+      description:
+        "Experience immersive sound with advanced noise-cancellation technology.",
+      imgSrc: "images/noise-cancelling.svg",
+    },
+  ];
+
+  // Populate content dynamically
+  function populateContent(hotspotDataItem, container) {
+    container.innerHTML = `
+            <img src="${hotspotDataItem.imgSrc}" alt="${hotspotDataItem.title}">
+            <h2>${hotspotDataItem.title}</h2> 
+            <p>${hotspotDataItem.description}</p> 
+            
+        `;
+  }
+
+  // Show info on hover
+  function showInfo(e) {
+    const slot = e.currentTarget.getAttribute("slot");
+    const hotspotDataItem = infoHotspot.find((item) => item.slot === slot);
+
+    if (hotspotDataItem) {
+      const infoContainer = e.currentTarget.querySelector(".HotspotAnnotation");
+      populateContent(hotspotDataItem, infoContainer);
+      gsap.to(infoContainer, { duration: 1, autoAlpha: 1 });
+    }
+  }
+
+  // Hide info function
+  function hideInfo(e) {
+    const infoContainer = e.currentTarget.querySelector(".HotspotAnnotation");
+    gsap.to(infoContainer, { duration: 1, autoAlpha: 0 });
+  }
+
+  const hotspots = document.querySelectorAll(".Hotspot");
+  hotspots.forEach((hotspot) => {
+    hotspot.addEventListener("mouseover", showInfo);
+    hotspot.addEventListener("mouseout", hideInfo);
+  });
+})();
